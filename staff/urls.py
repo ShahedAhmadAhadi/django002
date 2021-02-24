160854ef3708fd08ee44c97586c58bacf649ee0f
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-24 19:48:31 +0430

    application basic functionality completed

--- staff/urls.py
+++ staff/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.worker_view),
+    path('', views.worker_view, name="main"),
     path('<int:w_id>/', views.detail, name="detail")
 
 ]
