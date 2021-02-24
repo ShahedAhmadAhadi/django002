cead0f8c5870b5c33d1b47117a2677df2b280316
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-24 10:12:58 +0430

    data retrieved from database to html

--- staff/views.py
+++ staff/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import path
+from .models import worker
 
 # Create your views here.
 
 def worker_view(request):
+    workers = worker.objects.all()
+    context ={
+        'workers': workers
+    }
 
-    return render(request, './staff/index.html')
+    return render(request, './staff/index.html', context)
     
