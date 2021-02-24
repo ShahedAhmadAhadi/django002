5e924bf7233cd8cb156b0950b8f566155e5a9abf
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-24 10:29:53 +0430

    url for workers entered

--- staff/urls.py
+++ staff/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.worker_view)
-    
+    path('', views.worker_view),
+    path('<int:w_id>/', views.detail, name="detail")
 
 ]
--- staff/views.py
+++ staff/views.py
@@ -8,8 +8,16 @@ from .models import worker
 def worker_view(request):
     workers = worker.objects.all()
     context ={
-        'workers': workers
+        'workers': workers,
     }
 
     return render(request, './staff/index.html', context)
+
+def detail(request, w_id):
+    worker_detail = worker.objects.get(pk=w_id)
+    context = {
+        'detail': worker_detail,
+    }
+    return render(request, './staff/index.html', context)
+
     
